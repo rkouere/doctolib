20ea825e3bf51b935a5ea9b28186ad66fa65ceef
rkouere <rkouere@gmail.com>
2017-01-14 17:23:05 +0100

    now works with gmail

--- doctolib.py
+++ doctolib.py
@@ -29,7 +29,7 @@ def send_mail(sender, receivers, message, psswd):
     '''
     Sends a mail
     '''
-    server = smtplib.SMTP('smtps.univ-lille1.fr', 587)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(sender, psswd)
     server.sendmail(sender, receivers, message)
@@ -90,21 +90,20 @@ def main():
     content = get_page(url)
     next_appointement = get_next_appointement(content)
     appointement_wanted = datetime.strptime(arguments.date, '%d-%m-%Y')
-#
-#    auth_pwd = getpass('SMTP password for the sender')
-#
-#    while True:
-#        if(appointement_wanted > next_appointement):
-#            send_mail(sender, receivers, message, auth_pwd)
-#            print("appointement wanted = {}".format(appointement_wanted))
-#            print("next appointement = {}".format(next_appointement))
-#            sys.exit()
-#        else:
-#            content = get_page(url)
-#            next_appointement = datetime.strptime(
-#                get_next_appointement(content)[0][:-2], '%d %b %Y')
-#            print(next_appointement)
-#            time.sleep(randint(10, 60))
+
+    auth_pwd = getpass('SMTP password for the sender')
+
+    while True:
+        if(appointement_wanted > next_appointement):
+            send_mail(sender, receivers, message, auth_pwd)
+            print("appointement wanted = {}".format(appointement_wanted))
+            print("next appointement = {}".format(next_appointement))
+            sys.exit()
+        else:
+            content = get_page(url)
+            next_appointement = get_next_appointement(content)
+            print(next_appointement)
+            time.sleep(randint(10, 60))
 
 # This is a Python's special:
 # The only way to tell wether we are running the program as a binary,
